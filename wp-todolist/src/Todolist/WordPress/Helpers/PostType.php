a1dc965a3445013dffb1f31b7fb789b31f1ad6d1
Mouadh Aouiti <aouitimouadh@gmail.com>
2022-01-06 23:56:28 +0100

    initial commit

---
+++ wp-todolist/src/Todolist/WordPress/Helpers/PostType.php
@@ -0,0 +1,17 @@
+<?php
+// wp-todolist/src/Todolist/WordPress/Helpers/PostType.php
+
+namespace Todolist\WordPress\Helpers;
+
+/**
+ *
+ * @author Mouadh Aouiti
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+abstract class PostType{
+
+    const CPT_TODO = "td-todo";
+
+} 
+ 
---
+++ wp-todolist/wp-todolist.php
@@ -0,0 +1,9 @@
+<?php
+// wp-content/plugins/wp-todolist/wp-todolist.php
+
+/**
+ * Plugin Name: WP Todolist
+ * Description: Gestion d'une Todolist dans WordPress
+ * Version: 1.0.0
+ * Author: <a href="" target="_blank">Mouadh Aouiti</a>
+ */
